186302aec1f91df2e8e8dc6d65744f3669ebb401
Avinash Thalakola <avinashthalakola.offl@gmail.com>
2025-12-15 17:47:42 +0530

    Move MongoDB URI to environment variable

--- config/db.js
+++ config/db.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://avinashthalakolaoffl_db_user:Avinash1028@cluster0.mnrsxbu.mongodb.net/oilsales?appName=Cluster0';
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection failed:', err.message);
