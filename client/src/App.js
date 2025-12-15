b92c536dbc29b1dcd389a2e38e379c4e80e63260
Avinash Thalakola <avinashthalakola.offl@gmail.com>
2025-12-15 20:29:32 +0530

    Align API base URL import name

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import BASE_URL from "./config";
+import API_BASE_URL from "./config";
+
 
 function App() {
   const [isLogin, setIsLogin] = useState(true);
--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
-const BASE_URL =
+const API_BASE_URL =
   process.env.NODE_ENV === "production"
     ? "https://oilapp-42v8.onrender.com"
-    : "http://localhost:5001";
+    : "";
 
-export default BASE_URL;
+export default API_BASE_URL;
